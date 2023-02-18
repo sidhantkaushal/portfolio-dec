0d2c97a9d281c7b0f63c914565bba047fe8562d1
sidhantkaushal <kaushalsidhant434@gmail.com>
2023-02-19 02:08:47 +0530

    add skills projects and contact sections

--- js/main.js
+++ js/main.js
@@ -3,3 +3,52 @@ let menu = document.querySelector(".menu-icon");
 menu.onclick = () => {
   menu.classList.toggle("move");
 };
+
+// Email JS
+function validate() {
+  let name = document.querySelector(".name");
+  let email = document.querySelector(".email");
+  let msg = document.querySelector(".message");
+  let sendBtn = document.querySelector(".send-btn");
+
+  sendBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (name.value == "" || email.value == "" || msg.value == "") {
+      emptyerror();
+    } else {
+      sendmail(name.value, email.value, msg.value);
+      success();
+    }
+  });
+}
+validate();
+
+function sendmail(name, email, msg) {
+  emailjs.send("service_x1nag1g", "template_tjdmry8", {
+    from_name: email,
+    to_name: name,
+    message: msg,
+  });
+}
+
+function emptyerror() {
+  swal({
+    title: "Oh No...",
+    text: "Field cannot be empty!",
+    icon: "error",
+  });
+}
+
+function success() {
+  swal({
+    title: "Email sent successfully",
+    icon: "success",
+  });
+}
+
+// Header BackGround change on Scroll
+
+let header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+  header.classList.toggle("header-active", window.scrollY > 0);
+});
